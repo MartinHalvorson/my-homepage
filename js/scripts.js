1b7fdc5c4da2348d0f5de8f8bf100b7e9cb96b35
14mthalvorson <14mthalvorson@gmail.com>
2023-11-24 09:15:05 -0500

    no op

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,3 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
